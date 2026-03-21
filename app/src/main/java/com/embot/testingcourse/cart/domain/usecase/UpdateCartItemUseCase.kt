31bc0f11c161509dd2c02e8a7baf5c66ff84750f
Emilio Botier <70599464+EmilioBoti@users.noreply.github.com>
2026-03-21 13:10:33 +0100

    feat: added update cart item useCase

---
+++ app/src/main/java/com/embot/testingcourse/cart/domain/usecase/UpdateCartItemUseCase.kt
@@ -0,0 +1,35 @@
+package com.embot.testingcourse.cart.domain.usecase
+
+import com.embot.testingcourse.cart.domain.repository.CartItemRepository
+import com.embot.testingcourse.core.domain.model.AppError
+import com.embot.testingcourse.productList.domain.repository.ProductRepository
+import kotlinx.coroutines.flow.first
+import javax.inject.Inject
+
+class UpdateCartItemUseCase @Inject constructor(
+    private val cartItemRepository: CartItemRepository,
+    private val productRepository: ProductRepository
+) {
+
+    suspend operator fun invoke(productId: String, quantity: Int) {
+        if (quantity < 0) {
+            throw AppError.Validation.QuantityMustBePositive
+        }
+
+        if (quantity == 0) {
+            cartItemRepository.removeFromCart(productId)
+            return
+        }
+
+        val product = productRepository.getProductById(productId).first()
+            ?: throw AppError.NotFoundError
+
+        if (quantity > product.stock) {
+            throw AppError.Validation.InsufficientStock(product.stock)
+        }
+
+        cartItemRepository.updateQuantity(productId, quantity)
+
+    }
+
+}
