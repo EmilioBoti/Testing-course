c083160329789421e69a8e401eadb4024375dfab
Emilio Botier <erodriguez@napptilus.com>
2026-04-24 19:52:52 +0200

    feat: add test to ProductListViewModel

---
+++ app/src/test/java/com/embot/testingcourse/core/stubs/FailingProductRepositoryStub.kt
@@ -0,0 +1,20 @@
+package com.embot.testingcourse.core.stubs
+
+import com.embot.testingcourse.productList.domain.model.Product
+import com.embot.testingcourse.productList.domain.repository.ProductRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOf
+
+class FailingProductRepositoryStub(
+    private val exception: Throwable
+): ProductRepository {
+
+    override fun getProduct(): Flow<List<Product>> = flow { throw exception }
+
+    override fun getProductById(id: String): Flow<Product?> = flowOf()
+
+    override fun getProductsByIds(ids: Set<String>): Flow<List<Product>> = flowOf()
+
+    override suspend fun refreshPRoduct() {}
+}
